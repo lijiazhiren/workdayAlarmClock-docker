71d377bd7cad3043af00eb3b07415ca4ca04123c
111 <35790476+lijiazhiren@users.noreply.github.com>
2025-11-11 03:44:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@
 # -r: 只读模式 (防止上传和删除操作)
 # -c: 禁用 CGI
 # &: 在后台运行，使脚本可以继续执行主程序
-# /bin/busybox httpd -p 8088 -h /app -r -c &
+/bin/busybox httpd -p 8088 -h /app -r -c &
 
-# echo "BusyBox HTTPD 文件服务已在 8088 端口启动，服务目录为 /app (只读)"
+echo "BusyBox HTTPD 文件服务已在 8088 端口启动，服务目录为 /app (只读)"
 
 # 使用 exec 启动主程序，使其成为容器的 PID 1，确保容器保持在前台运行
 
